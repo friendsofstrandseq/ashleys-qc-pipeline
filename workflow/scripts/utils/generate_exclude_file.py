e696d1d46cd3578ed575d17b8958d0fddd5c2418
Thomas Weber <thomas.weber@embl.de>
2022-08-03 15:19:40 +0200

    Docs and cleaning

--- workflow/scripts/utils/generate_exclude_file.py
+++ workflow/scripts/utils/generate_exclude_file.py
@@ -6,12 +6,7 @@ import os, sys
 df_config_files = pd.read_csv(snakemake.input[0], sep="\t")
 
 # READ BAM FILE HEADER OF FIRST BAM IN THE PANDAS DF
-h = pysam.view("-H", "{path}/{sample}/all/{cell}.sort.mdup.bam".format(
-    path=df_config_files["Folder"][0],
-    sample=df_config_files["Sample"][0],
-    cell=df_config_files["Cell"][0],
-    )
-)
+h = pysam.view("-H", snakemake.input.bam[0])
 
 # h = pysam.view("-H", os.listdir(snakemake.input.bam + "selected")[0])
 h = [e.split("\t") for e in h.split("\n") if "@SQ" in e]
