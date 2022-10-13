d6e1ea51427d9f6579d2cbec1c803f2f785243ab
Thomas Weber <thomas.weber@embl.de>
2022-10-13 17:22:48 +0200

    Count module + input/output change + GC update

--- workflow/scripts/utils/dev_all_cells_correct.py
+++ workflow/scripts/utils/dev_all_cells_correct.py
@@ -5,4 +5,4 @@ df["prediction"] = 1
 df["probability"] = 1
 df.loc[df["cell"].str.contains("05"), "prediction"] = 0
 df.loc[df["cell"].str.contains("12"), "prediction"] = 0
-df.to_csv(snakemake.output.path, sep="\t", index=False)
+df.to_csv(snakemake.output.folder, sep="\t", index=False)
