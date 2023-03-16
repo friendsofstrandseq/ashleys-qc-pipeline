e574e564953290643f17363b7ad359c528713a5b
Thomas Weber <thomas.weber@embl.de>
2023-03-16 15:40:19 +0100

    2.0.1: Small update, minor fix for positive_control_bypass (static sample name remaning)

--- workflow/scripts/utils/positive_control_bypass_dev.py
+++ workflow/scripts/utils/positive_control_bypass_dev.py
@@ -2,23 +2,41 @@ import os, sys
 import pandas as pd
 import scipy
 
+pd.options.display.max_rows = 100
+
+
 # LOAD MOSAIC COUNTS INFO
+# counts = snakemake.input.counts
+# counts_df = pd.read_csv(counts, sep="\t", compression="gzip")
+# counts_df["cell"] = counts_df["cell"] + ".sort.mdup.bam"
+
+# # Groupby cell & sum reads
+# counts_gb_df = counts_df.groupby("cell")[["c", "w"]].sum()
+# counts_gb_df["nb_reads"] = counts_gb_df["c"] + counts_gb_df["w"]
+# info = "/scratch/tweber/DATA/MC_DATA/PAPER/LCL/counts/LCL.info_raw"
 info = snakemake.input.info
 info_df = pd.read_csv(info, sep="\t", skiprows=13)
 info_df["cell"] = info_df["cell"] + ".sort.mdup.bam"
 
 # Load ashleys predictions
+# labels = pd.read_csv(directory + "/cell_selection/labels_raw.tsv".format(sample=sample), sep="\t").sort_values(by="cell")
+# labels_path = "/scratch/tweber/DATA/MC_DATA/PAPER/LCL/cell_selection/labels_notebook.tsv"
 labels_path = snakemake.input.labels
 labels = pd.read_csv(labels_path, sep="\t").sort_values(by="cell")
+# sample = "RPE1-WT"
+# labels["sample"] = sample
 labels["sample"] = snakemake.wildcards.sample
-
+print(labels)
 # Retrieve correct prediction
 labels_corrected = labels.loc[labels["prediction"] == 1]
 
 # Merge counts & labels
+# labels_corrected = pd.merge(info_df.reset_index()[["cell", "good"]], labels_corrected, on="cell")
 labels_corrected = pd.merge(info_df[["cell", "good"]], labels_corrected, on="cell")
 # Compute z-score on reads nb
 labels_corrected["z_score"] = scipy.stats.zscore(labels_corrected["good"])
+print(labels_corrected)
+# exit()
 
 # Output, Correct outliers predictions & proba
 z_score_cutoff = 5
