2.1.0: multistep normalisation fix + negative control bypass checkpoint mosaic count to automatically symlink/copy non-empty BAM files Add empty fastq files as neg control test

workflow/scripts/utils/positive_negative_control_bypass.py
@@ -7,10 +7,23 @@ info = snakemake.input.info
 info_df = pd.read_csv(info, sep="\t", skiprows=13)
 info_df["cell"] = info_df["cell"] + ".sort.mdup.bam"
 
+
 # Load ashleys predictions
 labels_path = snakemake.input.labels
 labels = pd.read_csv(labels_path, sep="\t").sort_values(by="cell")
 labels["sample"] = snakemake.wildcards.sample
+print(labels)
+# Neg control with no reads
+info_df_neg_control = info_df.loc[info_df["mapped"] == 0]
+print(info_df_neg_control)
+if info_df_neg_control.shape[0] > 0:
+    info_df_neg_control["prediction"] = 0
+    info_df_neg_control["probability"] = 0
+    info_df_neg_control["sample"] = snakemake.wildcards.sample
+    labels_neg_control = info_df_neg_control[["cell", "prediction", "probability"]]
+    print(labels_neg_control)
+    labels = pd.concat([labels, labels_neg_control]).sort_values(by="cell")
+print(labels)
 
 # Retrieve correct prediction
 labels_corrected = labels.loc[labels["prediction"] == 1]
@@ -20,7 +33,7 @@ labels_corrected = pd.merge(info_df[["cell", "good"]], labels_corrected, on="cel
 # Compute z-score on reads nb
 labels_corrected["z_score"] = scipy.stats.zscore(labels_corrected["good"])
 
-print(labels_corrected )
+print(labels_corrected)
 
 # Output, Correct outliers predictions & proba
 z_score_cutoff = 5