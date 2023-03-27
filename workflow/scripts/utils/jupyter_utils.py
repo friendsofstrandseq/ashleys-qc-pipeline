2.1.0: multistep normalisation fix + negative control bypass checkpoint mosaic count to automatically symlink/copy non-empty BAM files Add empty fastq files as neg control test

workflow/scripts/utils/jupyter_utils.py
@@ -21,7 +21,14 @@ def get_ipysheet(cell_per_sample, sample, mosaic_info, ashleys_labels):
     mosaic_info_df = pd.read_csv(mosaic_info, sep="\t", skiprows=13)
     print(mosaic_info_df)
 
-    mosaic_info_df["%dupl"] = 100 * (mosaic_info_df["dupl"] / mosaic_info_df["mapped"])
+    # mosaic_info_df.loc[~np.isfinite(mosaic_info_df["dupl"])]["dupl"]
+
+    mosaic_info_df["%dupl"] = 100 * (
+        mosaic_info_df.loc[np.isfinite(mosaic_info_df["dupl"])]["dupl"]
+        / mosaic_info_df.loc[np.isfinite(mosaic_info_df["mapped"])]["mapped"]
+    )
+    mosaic_info_df["%dupl"] = mosaic_info_df["%dupl"].fillna(0)
+    # mosaic_info_df["%dupl"] = 100 * (mosaic_info_df["dupl"] / mosaic_info_df["mapped"])
     mosaic_info_df["%dupl"] = mosaic_info_df["%dupl"].round(0)
     mosaic_info_df["good"] = mosaic_info_df["good"].astype(str)
     print(mosaic_info_df)