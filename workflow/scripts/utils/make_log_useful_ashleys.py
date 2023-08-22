79ca0102aa4860c10cdebb5f8eea1d41f58ee9c3
Thomas Weber <thomas.weber@embl.de>
2023-08-22 13:21:57 +0200

    2.2.1: fixes

--- workflow/scripts/utils/make_log_useful_ashleys.py
+++ workflow/scripts/utils/make_log_useful_ashleys.py
@@ -1,7 +1,9 @@
 import sys, os
 
 
-def make_log_useful(log_path, status, config):
+def make_log_useful(log_path, status, config, config_definitions):
+    logs_processed_dir = "/".join(log_path.split("/")[:-1]) + "/processed_logs_for_mail/"
+    os.makedirs(logs_processed_dir, exist_ok=True)
     log_path_new = "/".join(log_path.split("/")[:-1]) + "/processed_logs_for_mail/" + log_path.split("/")[-1]
     error_buffer = []
     record = 0
@@ -25,12 +27,8 @@ def make_log_useful(log_path, status, config):
             else:
                 continue
 
-    with open(log_path_new, "w") as logfile:
-        _ = logfile.write("\n".join(error_buffer))
-        _ = logfile.write("\n\n")
-
     my_env = dict(os.environ)
-    with open(log_path_new, "a") as logfile:
+    with open(log_path_new, "w") as logfile:
         _ = logfile.write("=======[{}]=======\n".format(status))
         _ = logfile.write("\n===[{}]===\n".format("Infrastructure information"))
         _ = logfile.write("Host: {}\n".format(my_env.get("HOST", "N/A")))
@@ -41,20 +39,43 @@ def make_log_useful(log_path, status, config):
         _ = logfile.write("Screen: {}\n".format(my_env.get("STY", "N/A")))
         _ = logfile.write("Conda ENV: {}\n".format(my_env.get("CONDA_DEFAULT_ENV", "N/A")))
 
-        _ = logfile.write("\n===[{}]===\n".format("Workflow information"))
-        _ = logfile.write("smk-wf-catalog/ashleys-qc-pipeline v{version}\n".format(version=str(config["version"])))
-        _ = logfile.write("Folder to processed : {}\n".format(str(config["data_location"])))
-        _ = logfile.write("Multistep normalisation module : {}\n".format(str(config["multistep_normalisation"])))
-        # _ = logfile.write("Multistep normalisation for SV calling : {}\n".format(str(config["multistep_normalisation_for_SV_calling"])))
-        # _ = logfile.write("HGSVC based normalisation for SV calling : {}\n".format(str(config["hgsvc_based_normalisation"])))
-        _ = logfile.write("Binning window size : {}\n".format(str(config["window"])))
-        _ = logfile.write("List of chromosomes processed : {}\n".format(str(config["chromosomes"])))
-        _ = logfile.write("Reference genome selected : {}\n".format(config["reference"]))
-        _ = logfile.write("MultiQC : {}\n".format(config["MultiQC"]))
-        # _ = logfile.write("Split PDF QC plot : {}\n".format(config["split_qc_plot"]))
-        _ = logfile.write("\n")
+        # Define the categories
+        categories = {
+            "General": ["email", "data_location", "reference", "samples_to_process"],
+            "Ashleys-QC parameters": [
+                "input_bam_legacy",
+                "ashleys_pipeline",
+                "ashleys_pipeline_only",
+                "ashleys_threshold",
+                "hand_selection",
+                "MultiQC",
+            ],
+            "Counts processing": ["blacklist_regions", "window"],
+            "Normalization": [
+                "multistep_normalisation",
+                "multistep_normalisation_for_SV_calling",
+                "hgsvc_based_normalized_counts",
+            ],
+            "Advanced Settings": ["ashleys_threshold", "window", "chromosomes", "chromosomes_to_exclude"],
+            "Genecore": ["genecore", "genecore_date_folder", "genecore_prefix"],
+            "Downstream Modules": ["arbigent", "arbigent_bed_file", "scNOVA"],
+        }
 
-    return
+        _ = logfile.write("\n=========================\n===[CONFIG PARAMETERS]===\n=========================\n")
+        # Log the configuration
+        for category, keys in categories.items():
+            _ = logfile.write("\n===[{}]===\n".format(category))
+            for key in keys:
+                if key in config:
+                    _ = logfile.write("{}: {}\n".format(key, config[key]))
+
+    if error_buffer:
+        with open(log_path_new, "a") as logfile:
+            _ = logfile.write("\nErrors recorded (can be related to OOM & NFS issues)")
+            _ = logfile.write("\n".join(error_buffer))
+            _ = logfile.write("\n\n")
+
+    return log_path_new
 
 
 # if __name__ == "__main__":
